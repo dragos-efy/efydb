41897f99fcdba24a81a1dd39b315b4e0a9db1c1c
Bnyro <bnyro@tutanota.com>
2022-11-30 18:45:23 +0100

    add fields to the Theme

--- entities/theme.go
+++ entities/theme.go
@@ -4,10 +4,13 @@ import "gorm.io/gorm"
 
 type Theme struct {
 	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Tags        string `json:"tags"`
-	Screenshot  string `json:"screenshot"`
-	Uploaded    uint   `json:"uploaded"`
-	EfyVersion  uint   `json:"efy_version"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Tags        []string `json:"tags"`
+	Config      string   `json:"config"`
+	Screenshot  string   `json:"screenshot"`
+	Uploaded    uint     `json:"uploaded"`
+	EfyVersion  uint     `json:"efy_version"`
+	Likes       uint     `json:"likes"`
+	Approved    bool     `json:"approved"`
 }
