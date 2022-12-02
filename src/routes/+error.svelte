518ea82568e3f69cb7dc1a505d29689bf1cf6aea
Bnyro <bnyro@tutanota.com>
2022-12-02 15:56:19 +0100

    add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+  
+<section>
+    <h1>{$page.status}</h1>
+    <p>{$page?.error?.message}</p>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        min-height: 70vh;
+    }
+
+    section h1 {
+        font-size: 120rem;
+    }
+
+    section p {
+        font-size: 35rem;
+    }
+</style>
