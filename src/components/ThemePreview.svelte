17836dbdc42093c4ca1a4824dcdcbf58891d3201
Bnyro <bnyro@tutanota.com>
2022-12-02 15:26:09 +0100

    move themes into its own component

---
+++ src/components/ThemePreview.svelte
@@ -0,0 +1,27 @@
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="theme" on:click={onClick}>
+    <img src={theme.screenshot} alt={theme.name}>
+    <h4>{theme.title}</h4>
+    <h5>{theme.username}</h5>
+</div>
+
+<script lang="ts">
+    export let theme: any;
+
+    const onClick = () => {
+        
+    }
+</script>
+
+<style>
+    .theme {
+        background: var(--efy_bg1);
+        border-radius: var(--efy_radius);
+        padding: var(--efy_padding);
+        margin: var(--efy_padding);
+    }
+
+    .theme img {
+        aspect-ratio: 16 / 9;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <section>
 {#each themes as theme}
-    <div class="theme">
-        <img src={theme.screenshot} alt={theme.name}>
-        <h4>{theme.title}</h4>
-        <h5>{theme.username}</h5>
-    </div>
+    <ThemePreview theme={theme} />
 {/each}
 </section>
 <script type="ts">
+	import ThemePreview from "../components/ThemePreview.svelte";
 	import fetchJson from "../lib/fetchjs";
 
     let themes: any[] = [];
@@ -24,16 +21,5 @@
         display: grid;
         grid-template-columns: auto auto auto;
         flex-wrap: wrap;
-    }
-
-    .theme {
-        background: var(--efy_bg1);
-        border-radius: var(--efy_radius);
-        padding: var(--efy_padding);
-        margin: var(--efy_padding);
-    }
-
-    .theme img {
-        aspect-ratio: 16 / 9;
     }
 </style>
